ca030e919d3a2361fc4c282f930ee93665cb5ff0
juan c <juanchozass@gmail.com>
2023-02-01 22:34:40 +0100

    nuxi init plus eslint and prettier

---
+++ components/UpDownCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    {{ count }}
+  </h1>
+  <div>
+    <button @click="increaseCount">up</button>
+    <button @click="decreaseCount">down</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref<number>(0);
+
+function increaseCount() {
+  count.value += 1;
+}
+
+function decreaseCount() {
+  count.value -= 1;
+}
+</script>
